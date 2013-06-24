2e85d0ed59d721736469b8f27870ef55b2cbab9a
Ubuntu <ubuntu@ip-172-31-24-76.us-west-2.compute.internal>
2013-06-24 23:33:28 +0000

    my first commit (according to heroku instructions)

--- web.js
+++ web.js
@@ -3,7 +3,7 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send('Hello Koba! Ready for the rest of this course?');
 });
 
 var port = process.env.PORT || 5000;
